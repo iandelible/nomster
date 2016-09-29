98c20d3020ccb7a64680488d212e5572baae9307
Ian Pan <ianpan@hotmail.com>
2016-09-29 03:42:11 +0000

    form submission

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :places
-  
+  resources :places do
+    resources :comments, only: :create
+  end
   # Example resource route with options:
   #   resources :products do
   #     member do
