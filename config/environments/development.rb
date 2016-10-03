cd804de83b2ae4d4f1b799ebe46dd26830087318
Ian Pan <ianpan@hotmail.com>
2016-10-03 21:26:19 +0000

    typo in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-    address: "stmp.gmail.com",
+    address: "smtp.gmail.com",
     port: 587,
     domain: "example.com",
     authentication: "plain",
